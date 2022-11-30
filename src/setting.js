050de7c9d788f01cee7e9dd03bcf05301b0f67ad
hankhank10 <m.a.hankinson@gmail.com>
2022-11-30 08:56:09 +0000

    Update setting.js

--- src/setting.js
+++ src/setting.js
@@ -5,8 +5,8 @@ export const settingUI = () => {
             key: "LogSpotToken",
             type: "string",
             title: "Your LogSpot Token",
-            description: "To link with Spotify, visit https://logspot.top. Once you authenticate with Spotify, the site will generate a token. Enter it below.",
-            default: "6bd78d5a03364c44e5f620545b56eae63b49b9e2f595efa14ac26262cf3f4737fea5730678312963"
+            description: "To link with Spotify, visit https://logspot.top/  Once you authenticate with Spotify, the site will generate a token. Enter it below.",
+            default: ""
         },
         {
             key: "LogSpotHeading",
